06d4e22d40d374755e3db5ff4a9d6d4e369f7a02
flavia-vale <flavia.vale@usp.br>
2026-05-10 16:30:28 -0300

    chore(payments): ajustar preços de teste dos planos basic e pro

--- src/api/routes/admin.js
+++ src/api/routes/admin.js
@@ -12,7 +12,7 @@ const ROLE_PERMISSIONS = {
 }
 
 const PAID_PLANS = ['basic', 'pro']
-const PLAN_PRICES = { trial: 0, basic: 40, pro: 70 }
+const PLAN_PRICES = { trial: 0, basic: 1, pro: 2 }
 const EXPORT_LIMIT = 100
 const DEFAULT_BOOTSTRAP_ADMIN_EMAILS = ['flavia.vale@usp.br', 'flaviaroberta.1496@gmail.com', 'tacianeaas02@gmail.com']
 
