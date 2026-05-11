ca7462d95d0b65a36b4381c85f257c840db89cec
flavia-vale <flavia.vale@usp.br>
2026-05-11 10:28:52 -0300

    fix(seo): ajustar OG/Twitter por LP e definir outputFileTracingRoot

---
+++ dashboard/app/bot-ofertas-supermercado-whatsapp/page.js
@@ -0,0 +1,7 @@
+import { LpTemplate, getLpMetadata } from '../_lpShared'
+
+export const metadata = getLpMetadata('bot-ofertas-supermercado-whatsapp')
+
+export default function Page() {
+  return <LpTemplate slug="bot-ofertas-supermercado-whatsapp" />
+}
